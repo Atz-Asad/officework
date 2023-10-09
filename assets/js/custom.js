4b64fad5b4193ca453fa8d4f7698ee796c887cb2
asad <105508686+Atz-Asad@users.noreply.github.com>
2023-10-09 23:49:05 +0600

    home-4 banner section complete

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,23 +1,3 @@
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
-    slidesPerView: 1,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
-    spaceBetween: 10,
-    allowSlideNext: true,
-    allowSlidePrev: true,
-    navigation: {
-      prevEl:".swiper-button-prev",
-      nextEl: ".swiper-button-next",
-      
-    },
-    thumbs: {
-      swiper: swiper,
-    },
-});
-
 
 $(document).ready(function(){
   $('.rv-testimonial-slider-container').owlCarousel({
@@ -38,7 +18,73 @@ $(document).ready(function(){
     items: 3,
     dots: false,
     navText:['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],   
-})
+  })
+
+//  home-1 WORK SECTION
+var swiper = new Swiper(".rv_work_content_main", {
+  spaceBetween: 10,
+  slidesPerView: 1,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".rv_work_area", {
+  spaceBetween: 10,
+  allowSlideNext: true,
+  allowSlidePrev: true,
+  loop:true,
+  navigation: {
+    prevEl:".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+    
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
   
+//  home 4 banner section
+var swiper = new Swiper(".rv_4_banner_slider_bottom_area", {
+  spaceBetween: 10,
+  slidesPerView: 1,
+  freeMode: true,
+  loop: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".rv_4_banner_slider", {
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'custom', // Use custom pagination rendering
+    renderCustom: function (swiper, current, total) {
+      // Add a "0" prefix if the current page number is less than 10
+      var pageNumber = current < 9 ? '0' + (current + 1) : (current + 1);
+      return pageNumber; // Display the formatted page number
+    },
+  },
+  navigation: {
+    nextEl: ".rv_4_banner_slider_next",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+// home-4 progess start
+var forEach = function (array, callback, scope) {
+	for (var i = 0; i < array.length; i++) {
+		callback.call(scope, i, array[i]);
+	}
+};
+// window.onload = function(){
+	
+// }
+var max = -219.99078369140625;
+forEach(document.querySelectorAll('.progress'), function (index, value) {
+percent = value.getAttribute('data-progress');
+  value.querySelector('.fill').setAttribute('style', 'stroke-dashoffset: ' + ((100 - percent) / 100) * max);
+  // value.querySelector('.value').innerHTML = percent + '';
+});
+// home-4 progess end
 });
 
